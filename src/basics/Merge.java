e9462b9426f9fd64102b0e7634772b50e021c5a0
aniroger <anirudhsharma303@gmail.com>
2023-01-31 15:02:59 +0530

    Chnage in develop2

--- src/basics/Merge.java
+++ src/basics/Merge.java
@@ -5,7 +5,7 @@ public class Merge {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("anirudh");
+		System.out.println("an");
 
 		
 		System.out.println("hello anirudh");
